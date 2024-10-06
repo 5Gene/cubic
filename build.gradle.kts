aee6014e3af0a323c7f1f1d672271b7b82dbe6c2
ZuYun <jonas.jzy@gmail.com>
2024-10-07 01:13:03 +0800

    统一版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 //@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(wings.plugins.compose) apply false
+    alias(vcl.plugins.android.application) apply false
+    alias(vcl.plugins.kotlin.jvm) apply false
+    alias(vcl.plugins.kotlin.serialization) apply false
+    alias(vcl.plugins.gene.compose) apply false
+    alias(vcl.plugins.gene.android) apply false
+    alias(vcl.plugins.compose.compiler) apply false
 }
 
 //buildscript {
@@ -16,14 +18,5 @@ plugins {
 ////        set("kotlin_version", "1.7.10")
 ////    }
 //    println(project.extensions.getByName("ext"))
-//    repositories {
-//        google()
-//        mavenCentral()
-//    }
-//    dependencies {
-//        classpath(libs.android.gradle.plugin)
-//        classpath(libs.kotlin.gradle.plugin)
-//        // NOTE: Do not place your application dependencies here; they belong
-//        // in the individual module build.gradle files
-//    }
 //}
+//println(" ================================================= jspark >> $project ${project.ext} ${project.ext.properties}")
---
+++ cubic/build.gradle.kts
@@ -0,0 +1,19 @@
+import wing.publishMavenCentral
+
+plugins {
+    id("com.android.library")
+    alias(vcl.plugins.gene.android)
+}
+
+group = "io.github.5gene"
+version = "1.0"
+
+android {
+    namespace = "osp.sparkj.cubic"
+}
+
+publishMavenCentral("cubic", withSource = true)
+
+dependencies {
+    implementation(wings.gene.cartoon)
+}
