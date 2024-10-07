067d0f750841c5b433b662c079a885fc0c11b036
ZuYun <jonas.jzy@gmail.com>
2024-10-07 21:07:37 +0800

    统一版本

--- cubic/build.gradle.kts
+++ cubic/build.gradle.kts
@@ -3,6 +3,7 @@ import wing.publishMavenCentral
 plugins {
     id("com.android.library")
     alias(vcl.plugins.gene.android)
+    alias(vcl.plugins.compose.compiler)
 }
 
 group = "io.github.5gene"
